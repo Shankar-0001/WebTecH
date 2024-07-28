27ae9c5b844e1fbb50c8bbd808219951b1660022
Shankar-0001 <your-email@example.com>
2024-07-28 18:59:39 +0530

    menu added

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,26 +3,35 @@ import './home.css';
 
 export function Home() {
 
-    const handlMenuClick = () =>{
-       const navbar=document.querySelector('.navbar');
-       navbar.classList.toggle('active');
+    const handlMenuClick = () => {
+        const navbar = document.querySelector('.navbarTag');
+        navbar.classList.toggle('active');
     }
 
     return (
         <div>
-            <header>
-                <div>
-                    <Link to='#' className='logo'>WebTecH</Link>
+            <div className="HeadTag">
+                <div className='extra container p-0 my-0'>
+                    <div>
+                        <Link to='#' className='logo'>WebTecH</Link>
+                    </div>
+                    <div className='menuIcon' onClick={handlMenuClick}>&#9776;</div>
+                    <ul className='navbarTag'>
+                        <li><Link to='#' className='link'>Home</Link></li>
+                        <li><Link to='#' className='link'>About</Link></li>
+                        <li><Link to='#' className='link'>Programs</Link></li>
+                        <li><Link to='#' className='link'>Blogs</Link></li>
+                        <li><Link to='#' className='link'>Contact</Link></li>
+                    </ul>
+                    <ul className='navbarTag'>
+                        <li><Link to='#' className='link'><span  className='bi bi-instagram'></span></Link></li>
+                        <li><Link to='#' className='link'><span className='bi bi-instagram'></span></Link></li>
+                        <li><Link to='#' className='link'><span className='bi bi-instagram'></span></Link></li>
+                        <li><Link to='#' className='link'><span className='bi bi-instagram'></span></Link></li>
+                        <li><Link to='#' className='link'><span className='bi bi-instagram'></span></Link></li>
+                    </ul>
                 </div>
-                <div className='menuIcon' onClick={handlMenuClick}>&#9776;</div>
-                <ul className='navbar'>
-                    <li><Link to='#' className='link'>Home</Link></li>
-                    <li><Link to='#' className='link'>About</Link></li>
-                    <li><Link to='#' className='link'>Programs</Link></li>
-                    <li><Link to='#' className='link'>Blogs</Link></li>
-                    <li><Link to='#' className='link'>Contact</Link></li>
-                </ul>
-            </header>
+            </div>
             <div className='bgImage' style={{
                 height: "100vh",
                 width: "100%",
